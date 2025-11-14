a9337291fa435e1fdc7dd4a724dc1686659d610f
mehutonkka <mishka.hagelberg@gmail.com>
2025-11-14 11:19:02 +0200

    Refactor, rename and clean project

--- src/db_helper.py
+++ src/db_helper.py
@@ -3,8 +3,8 @@ from sqlalchemy import text
 import os
 
 def reset_db():
-  print(f"Clearing contents from table todos")
-  sql = text(f"DELETE FROM todos")
+  print(f"Clearing contents from table \"references\"")
+  sql = text(f"DELETE FROM \"references\"")
   db.session.execute(sql)
   db.session.commit()
 
---
+++ src/repositories/reference_repository.py
@@ -0,0 +1,14 @@
+from config import db
+from sqlalchemy import text
+
+from entities.reference import Reference
+
+def get_references():
+    result = db.session.execute(text('SELECT id, content FROM "references"'))
+    rows = result.fetchall()
+    return [Reference(r[0], r[1]) for r in rows]
+
+def create_reference(content):
+    sql = text('INSERT INTO "references" (content) VALUES (:content)')
+    db.session.execute(sql, { "content": content })
+    db.session.commit()
