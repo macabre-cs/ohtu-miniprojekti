9c44efd6145557f33e9fd90bc42ec03f18268c71
Kauri-Viljar <macabre.cs@gmail.com>
2025-11-16 11:19:55 +0200

    implement backend route for retriving references and basic form for creating references

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ from db_helper import reset_db
 from repositories.reference_repository import get_references, create_reference
 from config import app, test_env
 from util import validate_reference
+from entities.reference import Reference
 
 @app.route("/")
 def index():
@@ -16,14 +17,23 @@ def new():
 @app.route("/create_reference", methods=["POST"])
 def reference_creation():
     title = request.form.get("title")
+    cite_key = request.form.get("cite_key")
+    year = request.form.get("year")
+    publisher = request.form.get("publisher")
 
     try:
         validate_reference(title)
-        create_reference(title)
+        reference = Reference({
+            "cite_key": cite_key,
+            "title": title,
+            "year": year,
+            "publisher": publisher
+        })
+        create_reference(reference)
         return redirect("/")
     except Exception as error:
         flash(str(error))
-        return  redirect("/new_reference")
+        return redirect("/new_reference")
 
 # testausta varten oleva reitti
 if test_env:
