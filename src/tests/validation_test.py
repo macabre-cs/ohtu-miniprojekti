b0c94d586ddf3b4db03da83ad59f725165656d78
aarnioem <aarnioem@gmail.com>
2025-11-21 19:41:16 +0200

    updated validation tests and removed required publisher test

--- src/tests/validation_test.py
+++ src/tests/validation_test.py
@@ -6,35 +6,64 @@ class TestValidateReference(unittest.TestCase):
     def test_valid_input(self):
         # should not raise
         try:
-            validate_reference("key1", "Title", "Author", 2020, "Publisher")
+            validate_reference({
+                "reference_type": "book",
+                "cite_key": "key1",
+                "title": "Title",
+                "authors_formatted": "Author",
+                "year": 2020,
+                "publisher": "Publisher",
+            })
         except ValueError:
             self.fail("validate_reference raised ValueError unexpectedly!")
 
     def test_missing_cite_key(self):
         with self.assertRaises(ValueError) as context:
-            validate_reference("", "Title", "Author", 2020, "Publisher")
+            validate_reference({
+                "reference_type": "book",
+                "cite_key": "",
+                "title": "Title",
+                "authors_formatted": "Author",
+                "year": 2020,
+                "publisher": "Publisher",
+            })
         self.assertEqual(str(context.exception), "Cite key is required")
 
     def test_missing_title(self):
         with self.assertRaises(ValueError) as context:
-            validate_reference("key1", "", "Author", 2020, "Publisher")
+            validate_reference({
+                "reference_type": "book",
+                "cite_key": "key1",
+                "title": "",
+                "authors_formatted": "Author",
+                "year": 2020,
+                "publisher": "Publisher",
+            })
         self.assertEqual(str(context.exception), "Title is required")
 
     def test_missing_author(self):
         with self.assertRaises(ValueError) as context:
-            validate_reference("key1", "Title", "", 2020, "Publisher")
+            validate_reference({
+                "reference_type": "book",
+                "cite_key": "key1",
+                "title": "Title",
+                "authors_formatted": "",
+                "year": 2020,
+                "publisher": "Publisher",
+            })
         self.assertEqual(str(context.exception), "Author(s) is/are required")
 
     def test_invalid_year(self):
         with self.assertRaises(ValueError) as context:
-            validate_reference("key1", "Title", "Author", "abcd", "Publisher")
+            validate_reference({
+                "reference_type": "book",
+                "cite_key": "key1",
+                "title": "Title",
+                "authors_formatted": "Author",
+                "year": "abcd",
+                "publisher": "Publisher",
+            })
         self.assertEqual(str(context.exception), "Year must be a valid number")
-
-    def test_missing_publisher(self):
-        with self.assertRaises(ValueError) as context:
-            validate_reference("key1", "Title", "Author", 2020, "")
-        self.assertEqual(str(context.exception), "Publisher is required")
-
 
 if __name__ == "__main__":
     unittest.main()
