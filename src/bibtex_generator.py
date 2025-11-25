57c0b8258714f5063b24991724be927a3e799dd5
Eemil Aarnio <aarnioem@gmail.com>
2025-11-26 01:41:41 +0200

    fixed author formatting and removed empty fields

--- src/bibtex_generator.py
+++ src/bibtex_generator.py
@@ -14,9 +14,22 @@ def format_into_bibtex(reference):
 
     lines = [f"@{ref_type}{{{cite_key},"]
 
+    authors = reference.author.split(";")
+    formatted_authors = []
+
+    # muuttaa authorit bibtex formaattiin
+    for author in authors:
+        if "," in author:
+            lastname, firstname = author.split(",")
+            formatted_authors.append(f"{firstname.strip()} {lastname.strip()}")
+        else:
+            formatted_authors.append(author)
+
+    reference.author = " and ".join(formatted_authors)
+
     for key, value in reference:
-        if key not in ["reference_type", "cite_key", "id"]:
-            lines.append(f"  {key} = \"{value}\",")
+        if key not in ["reference_type", "cite_key", "id"] and value:
+            lines.append(f"  {key} = {{{value}}},")
 
     # Tämä poistaa pilkun viimeiseltä riviltä. Saa parantaa.
     lines[-1] = lines[-1][:-1]
